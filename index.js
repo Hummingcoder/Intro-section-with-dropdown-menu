1d8074ad21c2568be0287fd1b8fd4b9a7a195962
Vishal D. <dwivedivishal0702@gmail.com>
2024-07-30 08:09:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const toggleBtn = document.querySelector(".toggleBtn");
+const nav = document.querySelector(".navbar");
+const overlay = document.querySelector(".overlay");
+const dropdown_container = document.querySelectorAll(".dropdown_container");
+
+dropdown_container.forEach((drop) => {
+  const dropBtn = drop.querySelector(".dropbtn");
+  console.log(dropBtn);
+  drop.addEventListener("mouseover", () => {
+    drop.classList.add("show");
+  });
+  drop.addEventListener("mouseleave", () => {
+    drop.classList.remove("show");
+  });
+  drop.addEventListener("click", () => {
+    drop.classList.toggle("show");
+  });
+});
+
+toggleBtn.addEventListener("click", (e) => {
+  if (!nav.classList.contains("show")) {
+    nav.classList.add("show");
+    overlay.classList.add("show");
+  } else {
+    overlay.style.opacity = "0";
+    setTimeout(() => {
+      overlay.classList.remove("show");
+      overlay.style.opacity = "1";
+    }, 300);
+    nav.classList.remove("show");
+  }
+});
